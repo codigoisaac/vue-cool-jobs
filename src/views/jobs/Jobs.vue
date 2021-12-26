ed3323a94e0998a2dab1faf6c58ce815ce1a63ef
codigoisaac <codigoisaac@protonmail.com>
2021-12-26 20:33:50 -0300

    Accept route parameters as props

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -4,7 +4,7 @@
   <div class="jobs">
     <div v-for="job in jobs" :key="job.id" class="job">
       <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-        {{ job.title }}
+        <h2>{{ job.title }}</h2>
       </router-link>
     </div>
   </div>
@@ -15,9 +15,9 @@ export default {
   data() {
     return {
       jobs: [
-        { title: "Ninja UX Designer", id: 1, details: "lorem" },
-        { title: "Ninja Web Developer", id: 2, details: "lorem" },
-        { title: "Ninja Vue Developer", id: 3, details: "lorem" },
+        { title: "UX Designer", id: 1, details: "lorem" },
+        { title: "Web Developer", id: 2, details: "lorem" },
+        { title: "Vue Developer", id: 3, details: "lorem" },
       ],
     };
   },
@@ -27,5 +27,20 @@ export default {
 <style>
 .job {
   margin: 10px;
+}
+.job h2 {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
+}
+.job h2:hover {
+  background: #ddd;
+}
+.job a {
+  text-decoration: none;
 }
 </style>
